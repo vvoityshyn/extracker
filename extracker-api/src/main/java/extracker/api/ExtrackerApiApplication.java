a5690cc3b80d9982ba1f08fa5d5472bfb5bd2b9b
Volodymyr Voityshyn <voytyshyn@gmail.com>
2019-03-25 21:17:51 +0200

    Initial project structure

---
+++ extracker-api/src/main/java/extracker/api/ExtrackerApiApplication.java
@@ -0,0 +1,13 @@
+package extracker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtrackerApiApplication.class, args);
+	}
+
+}
