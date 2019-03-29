3c2aa76b080e717a431bd3575c22235952291456
Volodymyr Voityshyn <voytyshyn@gmail.com>
2019-03-29 09:34:12 +0200

    created the 'user' table

---
+++ extracker-db/extracker-create-db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "user"
+(
+	"id" SERIAL NOT NULL,
+	"email" VARCHAR(512) NOT NULL,
+	"password" VARCHAR(50) NOT NULL,
+	"first_name" VARCHAR(255) NOT NULL,
+	"surname" VARCHAR(255) NOT NULL,
+	"is_active" BOOLEAN NOT NULL,
+	CONSTRAINT pk_user_id PRIMARY KEY ("id")
+);
