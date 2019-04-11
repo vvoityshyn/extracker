19ab48a0d88ad37190a7211b281339b353f1b999
Volodymyr Voityshyn <voytyshyn@gmail.com>
2019-04-11 13:44:08 +0200

    Unique constraint for the 'email' attribute

--- extracker-db/extracker-create-db.sql
+++ extracker-db/extracker-create-db.sql
@@ -6,5 +6,6 @@ CREATE TABLE "user"
 	"first_name" VARCHAR(255) NOT NULL,
 	"surname" VARCHAR(255) NOT NULL,
 	"is_active" BOOLEAN NOT NULL,
-	CONSTRAINT pk_user_id PRIMARY KEY ("id")
+	CONSTRAINT pk_user_id PRIMARY KEY ("id"),
+	CONSTRAINT uk_email UNIQUE ("email")
 );
